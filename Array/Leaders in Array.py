ec8d99b7dc0ab187d98782c3a80ccc57d5c85f10
Kundan Kumar Roy <kkroy07@gmail.com>
2022-07-29 07:32:00 +0530

    Leaers in Array

---
+++ Array/Leaders in Array.py
@@ -0,0 +1,14 @@
+#Given an array A of positive integers. Your task is to find the leaders in the array.
+#An element of array is leader if it is greater than or equal to all the elements to its right side. The rightmost element is always a leader.
+def leaders(self, A, N):
+        #Code here
+        max=A[N-1]
+        revlead=[]
+        revlead.append(max)
+        for i in range(N-2,-1,-1):
+            if max <=A[i]:
+                max=A[i]
+                revlead.append(max)
+        revlead.reverse()
+        return revlead
+leaders([16,17,2,34,56],5)
